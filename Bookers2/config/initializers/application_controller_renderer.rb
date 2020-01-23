6ab8703121c7dc49bfc69dfef240f30223bffd61
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-01-23 12:27:07 +0900

    3rd commit

---
+++ Bookers2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'c7681961045996b9b1e9b596e98a483fd9a131a0e5bdddcc56e14e74a8bcde31a2c26b99644c6ccb6c3cfe407145c112543310e088e3790531d4039598cb76d9'
---
+++ Bookers2/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  get 'home/top'
+  get 'home/about'
+  devise_for :users
+  resources :books
+  resources :users
+  root 'home#top'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
